5d30f72bb4821deeaf89c908e415aac62b22ed6e
TIL-harrietowen <harriet.owen@theinformationlab.com>
2026-06-10 21:35:54 +0100

    session journey added to gold layer

--- amplitude_gold.sql
+++ amplitude_gold.sql
@@ -1,3 +1,5 @@
+//BUILD ALL_EVENTS
+---------------------------------------------------------
 create or replace table all_events as (
 
 with join_tables as (
@@ -11,6 +13,8 @@ with join_tables as (
         e.event_time,
         p.page_url,
         p.page_counter,
+        p.page_title,
+        p.parent_page_title,
         p.element_text,
         p.element_tag,
         p.element_url,
@@ -37,10 +41,8 @@ with join_tables as (
         row_number() over (partition by session_id order by event_time) as event_counter,
         page_url,
         page_counter,
-        (case
-            when page_url = 'https://www.theinformationlab.co.uk/' then 'The Information Lab - Home'
-            else replace(replace(page_url,'https://www.theinformationlab.co.uk/',''),'/',' - ')
-        end) as page_title,
+        page_title,
+        parent_page_title,
         element_text,
         element_tag,
         element_url,
@@ -60,4 +62,54 @@ order by session_id, event_time
 
 );
 
--- Check for duplicate event_uuids in dim_event_pages
+//BUILD SESSION_JOURNEY
+---------------------------------------------------------
+create or replace table session_journey as (
+
+with page_view_counts as (
+    select
+        e.session_id,
+        listagg(p.page_title,', ') as page_path,
+        count(*) as total_pages_viewed
+    from fct_all_session_events e
+    left join dim_event_pages p
+        on e.event_uuid = p.event_uuid
+    where e.event_type = 'Page Viewed'
+    group by e.session_id
+)
+
+, all_event_counts as (
+    select
+        e.session_id,
+        d.device_id,
+        d.device_type,
+        d.platform,
+        count(*) as total_events,
+        min(e.event_time) as session_start_time,
+        max(e.event_time) as session_end_time,
+        datediff('second',min(e.event_time),max(e.event_time)) as event_duration_s
+    from fct_all_session_events e
+    left join dim_devices d 
+        on e.device_id = d.device_id
+    group by e.session_id, d.device_id, d.device_type, d.platform     
+)
+
+,join_tables as (
+    select
+        e.session_id,
+        e.device_id,
+        e.device_type,
+        e.platform,
+        p.page_path,
+        e.total_events,
+        p.total_pages_viewed,
+        e.session_start_time,
+        e.session_end_time,
+        e.event_duration_s
+    from all_event_counts e
+    inner join page_view_counts p 
+    on e.session_id = p.session_id  
+)
+select * from join_tables
+
+);
